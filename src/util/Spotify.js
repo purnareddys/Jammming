03809a0472194a7cce5132351dc99e9ae19bf0ea
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-07-07 23:57:44 +0530

    final commit

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-const clientId = "4b06145452f743e2adef93873837d869"; // Insert client ID here.
+const clientId = ""; // Insert client ID here.
 const redirectUri = "http://localhost:3000"; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken;
 
