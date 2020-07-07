cebcb5f51f3839b101954d240f23cb8ed4f2aab2
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-07-07 20:04:40 +0530

    first commit

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Playlist.css";
+const Playlist = () => {
+  return (
+    <div>
+      <h1>Some content</h1>
+    </div>
+  );
+};
+export default Playlist;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchResults.css";
+const SearchResults = () => {
+  return (
+    <div>
+      <h1>Some content</h1>
+    </div>
+  );
+};
+export default SearchResults;
